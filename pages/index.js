423391d0d3d5c693dadb77ff50fd4bc86431fef8
Fernanda Rocha <frocha.angeles@gmail.com>
2025-06-13 19:39:11 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Perfil Innovador - Evaluación</title>
-        <meta name="description" content="Descubre tu estilo único de resolución de problemas" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Perfil Innovador</title>
       </Head>
       <main>
         <CPSAssessment />
